fc66008f72e5a1efd9d22ae6799a0e815897d072
Chandra <hoferc36@myumanitoba.ca>
2023-05-12 18:45:45 -0500

    resizing cell to fit screen. max of 21 cell for pixel 4a

--- app/src/main/java/com/example/mazeapp/MainActivity.kt
+++ app/src/main/java/com/example/mazeapp/MainActivity.kt
@@ -1,59 +1,48 @@
 package com.example.mazeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
-import com.example.mazeapp.databinding.*
+import android.util.DisplayMetrics
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mazeapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
 
-    private val rows = 19
-    private val cols = 10
+    private val rows = 5 //19
+    private val cols = 5 //21 //10
     private val board = Array(rows) { r ->
         IntArray(cols){ c ->
             (r * rows + c)
         }
     }
+    var cellSize = 100
+    val margin = 8
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind.main1)
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val height = (displayMetrics.heightPixels - margin*2) /rows
+        val width = (displayMetrics.widthPixels - margin*2) /cols
+        cellSize = if(width < height) width else height
+        cellSize = if(cellSize < 50) 50 else cellSize
+
         board.forEachIndexed{ rowIndex, row ->
             row.forEach{col ->
-                var cell = ImageView(this)
-                cell.minimumHeight = 100
-                cell.minimumWidth = 100
-                cell.x = 100F * col - 100F * rowIndex * rows
-                cell.y = 100F * rowIndex
-                R.drawable.cell
+                val cell = ImageView(this)
+                cell.minimumHeight = cellSize
+                cell.minimumWidth = cellSize
+                cell.x = 1F * cellSize * (col - rowIndex * rows) + margin
+                cell.y = 1F * cellSize * rowIndex + margin
                 cell.setBackgroundResource(R.drawable.cell)
                 //cell.setBackgroundResource(R.color.orange)
                 //cell.setImageDrawable(R.drawable.cell.toDrawable())
                 bind.main1.addView(cell)
             }
         }
-
-//        val cell1 = ImageView(this)
-//        cell1.minimumHeight = 100
-//        cell1.minimumWidth = 100
-//        cell1.x = 0f + 2 * 100f
-//        cell1.y = 0f
-//        cell1.setBackgroundResource(R.color.orange)
-////        cell1.setImageDrawable(R.drawable.cell.toDrawable())
-//        bind.main1.addView(cell1)
-//
-//
-//        val cell2 = ImageView(this)
-//        cell2.minimumHeight = 100
-//        cell2.minimumWidth = 100
-//        cell2.x = 100f
-//        cell2.y = 100f
-//        cell2.setBackgroundResource(R.color.orange)
-////        Log.i("chandra", Color.MAGENTA.toString())
-//
-////        cell2.setImageDrawable(Color.GREEN.toDrawable())
-//        bind.main1.addView(cell2)
     }
 }
