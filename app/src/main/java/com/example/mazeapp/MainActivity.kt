d9acbaad172725b230eb1328ebe5cb27d679e2b9
chofer04 <cmhoferfyi@hotmail.ca>
2023-05-15 17:26:56 -0500

    learnt how to access cell after creation

--- app/src/main/java/com/example/mazeapp/MainActivity.kt
+++ app/src/main/java/com/example/mazeapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mazeapp
 
-import android.graphics.Color
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.widget.ImageView
@@ -10,89 +9,120 @@ import com.example.mazeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
+    private lateinit var displayMetrics: DisplayMetrics
 
     private val rows = 5 //19
     private val cols = 5 //21 //10
     private val board = Array(rows) { r ->
         IntArray(cols){ c ->
-            (r * rows + c)
+            (r * rows + c + 1000)
         }
     }
     var cellSize = 50
     val margin = 8
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind.main1)
-        val displayMetrics = DisplayMetrics()
+
+        displayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        cellCreation()
+
+//        var cell = bind.main1.getChildAt(0)
+//        cell.setBackgroundResource(R.color.purple_1)
+//        bind.main1.getChildAt( rows * cols * 2 - 1).setBackgroundResource(R.color.white)
+    }
+
+    private fun cellCreation() {
         val height = (displayMetrics.heightPixels - margin*2) /rows
         val width = (displayMetrics.widthPixels - margin*2) /cols
         cellSize = if(width < height) width else height
         cellSize = if(cellSize < 50) 50 else cellSize
 
+//        var boardStatus = Array<ImageView>(board[rows][cols]){cell->
+//            cell = ImageView(this)
+//        }
+
         board.forEachIndexed{ rowIndex, row ->
             row.forEachIndexed{colIndex, col ->
-                val cell = ImageView(this)
-                cell.minimumHeight = cellSize
-                cell.minimumWidth = cellSize
-                cell.x = 1F * cellSize * (col - rowIndex * rows) + margin
-                cell.y = 1F * cellSize * rowIndex + margin
+                val cellBackground = ImageView(this)
+                cellBackground.minimumHeight = cellSize
+                cellBackground.minimumWidth = cellSize
+                cellBackground.x = 1F * cellSize * colIndex + margin
+                cellBackground.y = 1F * cellSize * rowIndex + margin
+//                cellBackground.id = col
+                if (rowIndex.mod(2) == 1){
+                    if(colIndex.mod(2) == 1) {
+                        cellBackground.setBackgroundResource(R.color.blank_cell)
+                    }else{
+                        cellBackground.setBackgroundResource(R.color.orange)
+                    }
+                }else{
+                    if(colIndex.mod(2) == 1) {
+                        cellBackground.setBackgroundResource(R.color.dark_orange)
+                    }else{
+                        cellBackground.setBackgroundResource(R.color.light_orange)
+                    }
+                }
+                bind.main1.addView(cellBackground)
 
-                cell.setBackgroundColor(Color.RED)
+                val cellForeground = ImageView(this)
+                cellForeground.minimumHeight = cellSize
+                cellForeground.minimumWidth = cellSize
+                cellForeground.x = 1F * cellSize * colIndex + margin
+                cellForeground.y = 1F * cellSize * rowIndex + margin
                 when (rowIndex) {
                     0 -> {
                         when (colIndex) {
                             0 -> {
-                                cell.setBackgroundResource(R.drawable.cell_2path_right_bottom)
+                                cellForeground.setBackgroundResource(R.drawable.cell_2path_right_bottom)
                             }
 
-                            cols-1 -> {
-                                cell.setBackgroundResource(R.drawable.cell_2path_left_bottom)
+                            cols - 1 -> {
+                                cellForeground.setBackgroundResource(R.drawable.cell_2path_left_bottom)
                             }
 
                             else -> {
-                                cell.setBackgroundResource(R.drawable.cell_3path_notop)
+                                cellForeground.setBackgroundResource(R.drawable.cell_3path_notop)
                             }
                         }
                     }
-                    rows-1 -> {
+
+                    rows - 1 -> {
                         when (colIndex) {
                             0 -> {
-                                cell.setBackgroundResource(R.drawable.cell_2path_top_right)
+                                cellForeground.setBackgroundResource(R.drawable.cell_2path_top_right)
                             }
 
-                            cols-1 -> {
-                                cell.setBackgroundResource(R.drawable.cell_2path_top_left)
+                            cols - 1 -> {
+                                cellForeground.setBackgroundResource(R.drawable.cell_2path_top_left)
                             }
 
                             else -> {
-                                cell.setBackgroundResource(R.drawable.cell_3path_nobottom)
+                                cellForeground.setBackgroundResource(R.drawable.cell_3path_nobottom)
                             }
                         }
                     }
+
                     else -> {
                         when (colIndex) {
                             0 -> {
-                                cell.setBackgroundResource(R.drawable.cell_3path_noleft)
+                                cellForeground.setBackgroundResource(R.drawable.cell_3path_noleft)
                             }
 
-                            cols-1 -> {
-                                cell.setBackgroundResource(R.drawable.cell_3path_noright)
+                            cols - 1 -> {
+                                cellForeground.setBackgroundResource(R.drawable.cell_3path_noright)
                             }
 
                             else -> {
-                                cell.setBackgroundResource(R.drawable.cell_4path)
+                                cellForeground.setBackgroundResource(R.drawable.cell_4path)
                             }
                         }
                     }
                 }
-//                cell.setBackgroundResource(R.drawable.cell)
-//                cell.setBackgroundResource(R.drawable.cell)
-//                cell.setBackgroundResource(R.color.orange)
-                //cell.setImageDrawable(R.drawable.cell.toDrawable())
-                bind.main1.addView(cell)
+                bind.main1.addView(cellForeground)
             }
         }
     }
