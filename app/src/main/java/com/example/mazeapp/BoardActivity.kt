86789565f01bd67b2db3f7c6518a1f0efb1ddaa1
chofer04 <cmhoferfyi@hotmail.ca>
2023-05-30 14:21:02 -0500

    fix margins, centered maze in width, remove double code Complete refactor to use a BoardMaze object for board operation

--- app/src/main/java/com/example/mazeapp/BoardActivity.kt
+++ app/src/main/java/com/example/mazeapp/BoardActivity.kt
@@ -3,6 +3,7 @@ package com.example.mazeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.*
 import android.widget.*
 import androidx.core.view.GestureDetectorCompat
@@ -17,18 +18,9 @@ class BoardActivity : AppCompatActivity() {
     private lateinit var gestureDetector: GestureDetectorCompat
 
     private var cellSize = 5
-    private val margin = 8
-    private var rows = 2
-    private var cols = 2
-    private lateinit var board: Array<Array<CellPieces>>
-    private lateinit var stack: Stack<Pair<Int, Int>>
-    private var visitedCellCount = 0
+    private val marginDP = 8
 
-    private val startCellCoords = Pair(0,0)
-    private var endCellCoords = Pair(0,0)
-
-    private var hereCell = CellPieces()
-    private var hereCoords = startCellCoords
+    private lateinit var boardMaze: BoardMaze
 
 //    private lateinit var upButton: Button
 //    private lateinit var leftButton: Button
@@ -40,9 +32,11 @@ class BoardActivity : AppCompatActivity() {
         bind = ActivityBoardBinding.inflate(layoutInflater)
         setContentView(bind.board1)
 
+        boardMaze = BoardMaze(intent.getIntExtra("heightMaze", 5),
+            intent.getIntExtra("widthMaze", 5), this)
+//        boardMaze.startCellCoords = Pair(1,0)
+//        boardMaze.endCellCoords = Pair(1, 1)
         cellCreation()
-        boardCreation()
-
         gestureDetector = GestureDetectorCompat(this, GestureListener1())
 
 //        upButton = bind.buttonUp
@@ -55,20 +49,36 @@ class BoardActivity : AppCompatActivity() {
 //        rightButton.setOnClickListener{moveRight()}
 //        downButton.setOnClickListener{moveDown()}
     }
+    fun endGame(){
+        Toast.makeText(applicationContext, "You WIN", Toast.LENGTH_SHORT).show()
+        finish()
+    }
+
+    private fun boardRefresh() {
+        boardMaze.board.forEach { row ->
+            row.forEach { cell ->
+                setCellBackgroundColor(cell)
+            }
+        }
+    }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         return when (keyCode) {
             KeyEvent.KEYCODE_DPAD_UP -> {
-                moveUp()
+                boardMaze.moveUp()
+                boardRefresh()
                 true
             }KeyEvent.KEYCODE_DPAD_LEFT -> {
-                moveLeft()
+                boardMaze.moveLeft()
+                boardRefresh()
                 true
             }KeyEvent.KEYCODE_DPAD_RIGHT -> {
-                moveRight()
+                boardMaze.moveRight()
+                boardRefresh()
                 true
             }KeyEvent.KEYCODE_DPAD_DOWN -> {
-                moveDown()
+                boardMaze.moveDown()
+                boardRefresh()
                 true
             }else -> { super.onKeyDown(keyCode, event)}
         }
@@ -82,227 +92,40 @@ class BoardActivity : AppCompatActivity() {
         }
     }
 
-    private fun moveUp(){
-        if (hereCell.top) {
-            hereCell.here = false
-            setCellBackgroundColor(hereCell)
-            hereCoords = Pair(hereCoords.first - 1, hereCoords.second)
-            hereCell = board[hereCoords.first][hereCoords.second]
-            hereCell.here = true
-            hereCell.visited = true
-            setCellBackgroundColor(hereCell)
-            if (hereCell.end) {
-                Toast.makeText(applicationContext, "You WIN", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-            if(hereCell.top && !hereCell.left && !hereCell.right && !hereCell.bottom){
-                moveUp()
-            }
-        }
-    }
-    private fun moveLeft(){
-        if(hereCell.left){
-            hereCell.here = false
-            setCellBackgroundColor(hereCell)
-            hereCoords = Pair(hereCoords.first, hereCoords.second-1)
-            hereCell = board[hereCoords.first][hereCoords.second]
-            hereCell.here = true
-            hereCell.visited = true
-            setCellBackgroundColor(hereCell)
-            if(hereCell.end){
-                Toast.makeText(applicationContext, "You win", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-            if(!hereCell.top && hereCell.left && !hereCell.right && !hereCell.bottom){
-                moveLeft()
-            }
-        }
-    }
-    private fun moveRight(){
-        if(hereCell.right){
-            hereCell.here = false
-            setCellBackgroundColor(hereCell)
-            hereCoords = Pair(hereCoords.first, hereCoords.second+1)
-            hereCell = board[hereCoords.first][hereCoords.second]
-            hereCell.here = true
-            hereCell.visited = true
-            setCellBackgroundColor(hereCell)
-            if(hereCell.end){
-                Toast.makeText(applicationContext, "You win", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-            if(!hereCell.top && !hereCell.left && hereCell.right && !hereCell.bottom){
-                moveRight()
-            }
-        }
-    }
-
-    private fun moveDown(){
-        if(hereCell.bottom){
-            hereCell.here = false
-            setCellBackgroundColor(hereCell)
-            hereCoords = Pair(hereCoords.first+1, hereCoords.second)
-            hereCell = board[hereCoords.first][hereCoords.second]
-            hereCell.here = true
-            hereCell.visited = true
-            setCellBackgroundColor(hereCell)
-            if(hereCell.end){
-                Toast.makeText(applicationContext, "You win", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-            if(!hereCell.top && !hereCell.left && !hereCell.right && hereCell.bottom){
-                moveDown()
-            }
-        }
-    }
-
-    private fun boardCreation() {
-        //Start and end points set
-        hereCell = board[startCellCoords.first][startCellCoords.second]
-        hereCell.start = true
-        hereCell.here = true
-        hereCell.visited = true
-        setCellBackgroundColor(hereCell)
-
-        endCellCoords = Pair(rows - 1, cols - 1)
-        board[endCellCoords.first][endCellCoords.second].end = true
-
-        //stack initialization
-        stack = Stack<Pair<Int, Int>>()
-        stack.push(hereCoords)
-        visitedCellCount = 1
-
-//        for(i in 1..rows*cols*2){
-//            Handler().postDelayed({
-//            if(visitedCellCount < rows * cols) {
-
-        //make a maze path until visited count == maze size
-        while (visitedCellCount < rows * cols) {
-                //selects next cell
-                pathSelector(hereCoords.first, hereCoords.second)
-                hereCell.here = false
-                setCellBackgroundColor(hereCell)
-                hereCoords = stack.peek()//hereCoord.first = row, hereCoord.second = col
-                hereCell = board[hereCoords.first][hereCoords.second]
-                hereCell.here = true
-                hereCell.visited = true
-                setCellBackgroundColor(hereCell)
-//            }},50L*i)
-        }
-        //return to start
-        hereCell.here = false
-
-        //reset all visited cells to unvisited
-        board.forEach { row ->
-            row.forEach { cell ->
-                cell.visited = false
-                setCellBackgroundColor(cell)
-            }
-        }
-
-        hereCoords = startCellCoords
-        hereCell = board[hereCoords.first][hereCoords.second]
-        hereCell.here = true
-        hereCell.visited = true
-        setCellBackgroundColor(hereCell)
-
-    }
-
-    private fun pathSelector(row:Int, col:Int) {
-        val availablePaths = mutableListOf<PATH>()
-
-        //check in the path is to the border of the cell is visited
-        //add it to a list to randomly chose
-        if (row > 0 && !board[row - 1][col].visited) {
-            availablePaths.add(PATH.TOP)
-        }
-        if (row < rows-1 && !board[row + 1][col].visited) {
-            availablePaths.add(PATH.BOTTOM)
-        }
-        if (col > 0 && !board[row][col - 1].visited) {
-            availablePaths.add(PATH.LEFT)
-        }
-        if (col < cols-1 && !board[row][col + 1].visited) {
-            availablePaths.add(PATH.RIGHT)
-        }
-
-        //path selector or to pop of stack to find a new path
-        if (availablePaths.size > 0) {
-            when (availablePaths[Random.nextInt(0, availablePaths.size)]) {
-                PATH.TOP -> {
-                    board[row][col].top = true
-                    board[row - 1][col].bottom = true
-                    stack.push(Pair(row - 1, col))
-                    cellOrientation(board[row - 1][col])
-                }
-                PATH.LEFT -> {
-                    board[row][col].left = true
-                    board[row][col - 1].right = true
-                    stack.push(Pair(row, col - 1))
-                    cellOrientation(board[row][col - 1])
-                }
-                PATH.RIGHT -> {
-                    board[row][col].right = true
-                    board[row][col + 1].left = true
-                    stack.push(Pair(row, col + 1))
-                    cellOrientation(board[row][col + 1])
-                }
-                PATH.BOTTOM -> {
-                    board[row][col].bottom = true
-                    board[row + 1][col].top = true
-                    stack.push(Pair(row + 1, col))
-                    cellOrientation(board[row + 1][col])
-                }
-            }
-            cellOrientation(board[row][col])
-            visitedCellCount++
-        }else{
-            if(!stack.empty()){
-                stack.pop()
-            }
-        }
-    }
-
     private fun cellCreation() {
-        //get the size of the screen
         displayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
 
-        //get input value from user
-        rows = intent.getIntExtra("heightMaze", 5)
-        cols = intent.getIntExtra("widthMaze", 5)
+//        val dpi = displayMetrics.densityDpi
+        val marginP = marginDP * (displayMetrics.densityDpi/160)
 
-        //initialize maze array
-        board = Array(rows) { Array(cols) { CellPieces() } }
+//        val heightDP = displayMetrics.heightPixels * 160 / dpi
+//        val heightP = displayMetrics.heightPixels
+        var heightButtons = 50 * (displayMetrics.densityDpi/160)
+        heightButtons = 0
+        val heightRow = (displayMetrics.heightPixels - marginP*2 - heightButtons*3) /boardMaze.rows
+        val widthCol = displayMetrics.widthPixels /boardMaze.cols
+        cellSize = if (widthCol < heightRow) widthCol else heightRow
+        //centre maze
+        val marginPwidth =  (displayMetrics.widthPixels - cellSize*boardMaze.cols)/2
 
-        //set the size of cells, cells are square
-        var height = (displayMetrics.heightPixels - 200)/rows
-        height = if(height < 10) 10 else height
-        var width = (displayMetrics.widthPixels - 100) /cols
-        width = if (width < 10) 10 else width
-        cellSize = if (width < height) width else height
+//        Log.d("chandra", "mdp:$marginDP mp:$marginP w:$displayMetrics.widthPixels cw:$cellSize")//maxWidth
 
-//        Log.d("chandra", "h:"+ height+" w:"+ width+" c:"+ cellSize)//maxWidth
-
-        //creates view for foreground(borders) and view background color for each cell
-        board.forEachIndexed { rowIndex, row ->
-            row.forEachIndexed { colIndex, cell ->
-                cell.position = rowIndex * cols + colIndex
-                cell.coords = Pair(rowIndex, colIndex)
-
+        boardMaze.board.forEach {row ->
+            row.forEach {cell ->
                 val cellBackground = ImageView(this)
                 cellBackground.minimumHeight = cellSize
                 cellBackground.minimumWidth = cellSize
-                cellBackground.y = 1F * cellSize * cell.coords.first + margin
-                cellBackground.x = 1F * cellSize * cell.coords.second + margin
+                cellBackground.y = 1F * cellSize * cell.coords.first + marginP
+                cellBackground.x = 1F * cellSize * cell.coords.second + marginPwidth
                 bind.boardBackground.addView(cellBackground)
                 setCellBackgroundColor(cell)
 
                 val cellForeground = ImageView(this)
                 cellForeground.minimumHeight = cellSize
                 cellForeground.minimumWidth = cellSize
-                cellForeground.y = 1F * cellSize * cell.coords.first + margin
-                cellForeground.x = 1F * cellSize * cell.coords.second + margin
+                cellForeground.y = 1F * cellSize * cell.coords.first + marginP
+                cellForeground.x = 1F * cellSize * cell.coords.second + marginPwidth
                 bind.boardForeground.addView(cellForeground)
                 cellOrientation(cell)
             }
@@ -409,9 +232,11 @@ class BoardActivity : AppCompatActivity() {
                 // this is a left or right swipe
                 if (abs(diffX) > SWIPE_THRESHOLD && abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
                     if (diffX > 0 ) {
-                        moveRight()
+                        boardMaze.moveRight()
+                        boardRefresh()
                     } else {
-                        moveLeft()
+                        boardMaze.moveLeft()
+                        boardRefresh()
                     }
                     true
                 } else  {
@@ -421,9 +246,11 @@ class BoardActivity : AppCompatActivity() {
                 // this is either a bottom or top swipe.
                 if (abs(diffY) > SWIPE_THRESHOLD && abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
                     if (diffY > 0) {
-                        moveDown()
+                        boardMaze.moveDown()
+                        boardRefresh()
                     } else {
-                        moveUp()
+                        boardMaze.moveUp()
+                        boardRefresh()
                     }
                     true
                 } else {
---
+++ app/src/main/java/com/example/mazeapp/BoardMaze.kt
@@ -0,0 +1,167 @@
+package com.example.mazeapp
+
+import java.util.Stack
+import kotlin.random.Random
+
+class BoardMaze (val rows:Int = 2, val cols:Int = 2, val activity: BoardActivity) {
+    val board: Array<Array<CellPieces>> = Array(rows) { Array(cols) { CellPieces() } }
+
+    private val stack: Stack<Pair<Int, Int>> = Stack<Pair<Int, Int>>()
+    private var visitedCellCount = 0
+
+    private var hereCoords = Pair(0,0)
+    private var hereCell = CellPieces()
+
+    var startCellCoords: Pair<Int,Int> = Pair(0,0)
+        set(value) {
+            if(value.first in 0 until rows && value.second in 0 until cols) {
+                board[startCellCoords.first][startCellCoords.second].start = false
+                hereCell.here = false
+                hereCell.visited = false
+                field = value
+                hereCoords = value
+                hereCell = board[hereCoords.first][hereCoords.second]
+                hereCell.start = true
+                hereCell.here = true
+                hereCell.visited = true
+            }
+        }
+    var endCellCoords = Pair(0, 0)
+        set(value) {
+            if(value.first in 0 until rows && value.second in 0 until cols) {
+                board[endCellCoords.first][endCellCoords.second].end = false
+                field = value
+                board[value.first][value.second].end = true
+            }
+
+        }
+
+    init {
+        board.forEachIndexed() { rowIndex, row ->
+            row.forEachIndexed { colIndex, cell ->
+                cell.position = rowIndex * cols + colIndex
+                cell.coords = Pair(rowIndex, colIndex)
+            }
+
+        }
+        hereCell = board[startCellCoords.first][startCellCoords.second]
+        hereCell.start = true
+        hereCell.here = true
+        hereCell.visited = true
+
+        endCellCoords = Pair(rows - 1, cols - 1)
+        board[endCellCoords.first][endCellCoords.second].end = true
+
+        stack.push(hereCoords)
+        visitedCellCount = 1
+        while (visitedCellCount < rows * cols) {
+            pathSelector(hereCoords.first, hereCoords.second)
+            hereCell.here = false
+            hereCoords = stack.peek()//hereCoord.first = row, hereCoord.second = col
+            hereCell = board[hereCoords.first][hereCoords.second]
+            hereCell.here = true
+            hereCell.visited = true
+        }
+        hereCell.here = false
+
+        board.forEach { row ->
+            row.forEach { cell ->
+                cell.visited = false
+            }
+        }
+
+        hereCoords = Pair(0,0)
+        hereCell = board[hereCoords.first][hereCoords.second]
+        hereCell.here = true
+        hereCell.visited = true
+    }
+    private fun pathSelector(row:Int, col:Int) {
+        val availablePaths = mutableListOf<PATH>()
+
+        if (row > 0 && !board[row - 1][col].visited) {
+            availablePaths.add(PATH.TOP)
+        }
+        if (row < rows-1 && !board[row + 1][col].visited) {
+            availablePaths.add(PATH.BOTTOM)
+        }
+        if (col > 0 && !board[row][col - 1].visited) {
+            availablePaths.add(PATH.LEFT)
+        }
+        if (col < cols-1 && !board[row][col + 1].visited) {
+            availablePaths.add(PATH.RIGHT)
+        }
+
+        if (availablePaths.size > 0) {
+            when (availablePaths[Random.nextInt(0, availablePaths.size)]) {
+                PATH.TOP -> {
+                    board[row][col].top = true
+                    board[row - 1][col].bottom = true
+                    stack.push(Pair(row - 1, col))
+                }
+                PATH.LEFT -> {
+                    board[row][col].left = true
+                    board[row][col - 1].right = true
+                    stack.push(Pair(row, col - 1))
+                }
+                PATH.RIGHT -> {
+                    board[row][col].right = true
+                    board[row][col + 1].left = true
+                    stack.push(Pair(row, col + 1))
+                }
+                PATH.BOTTOM -> {
+                    board[row][col].bottom = true
+                    board[row + 1][col].top = true
+                    stack.push(Pair(row + 1, col))
+                }
+            }
+            visitedCellCount++
+        }else{
+            if(!stack.empty()){
+                stack.pop()
+            }
+        }
+    }
+    fun moveUp(){
+        if (hereCell.top) {
+            moveCharacter(hereCoords.first-1, hereCoords.second)
+            if(hereCell.top && !hereCell.left && !hereCell.right){
+                moveUp()
+            }
+        }
+    }
+    fun moveLeft(){
+        if(hereCell.left){
+            moveCharacter(hereCoords.first, hereCoords.second-1)
+            if(!hereCell.top && hereCell.left && !hereCell.bottom){
+                moveLeft()
+            }
+        }
+    }
+    fun moveRight(){
+        if(hereCell.right){
+            moveCharacter(hereCoords.first, hereCoords.second+1)
+            if(!hereCell.top && hereCell.right && !hereCell.bottom){
+                moveRight()
+            }
+        }
+    }
+
+    fun moveDown(){
+        if(hereCell.bottom){
+            moveCharacter(hereCoords.first+1, hereCoords.second)
+            if(!hereCell.left && !hereCell.right && hereCell.bottom){
+                moveDown()
+            }
+        }
+    }
+    private fun moveCharacter(newRow:Int, newCol:Int){
+        hereCell.here = false
+        hereCoords = Pair(newRow, newCol)
+        hereCell = board[hereCoords.first][hereCoords.second]
+        hereCell.here = true
+        hereCell.visited = true
+        if (hereCell.end) {
+            activity.endGame()
+        }
+    }
+}
